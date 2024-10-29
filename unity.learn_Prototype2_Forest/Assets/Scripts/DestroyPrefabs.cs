971a1af972d54bde91b814f6df60b866f8c68eed
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-10-29 14:26:04 +0300

    Destroy prefabs from the scene by coding

---
+++ unity.learn_Prototype2_Forest/Assets/Scripts/DestroyPrefabs.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPrefabs : MonoBehaviour
+{
+
+    private float topBound = 30.0f;
+    private float floorBound = -10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < floorBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
